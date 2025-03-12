585f3783b3457fddae259cc3207714a351e5a300
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-03-12 13:00:25 +0530

    slider done

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,40 @@ const navbarbtn = () => {
     document.getElementById("rotates-2").classList.toggle("rotate-43")
     document.body.classList.toggle("overflow-hidden")
 }
+
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
+    autoplay: true,
+    autoplaySpeed: 800,
+    pauseOnMouseEnter: true,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
